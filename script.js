b0af2794745658fd8075b7439897b338e55591e2
Timsalabim1991 <71948868+Timsalabim1991@users.noreply.github.com>
2020-12-06 22:23:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Tim";
+
+// question 3
+var x = "12";
+
+// question 4
+var division = 20 / 5;
+
+// question 5
+var animal1 = "frog";
+console.log (animal1 === "frog");
+
+// question 6
+var orderHasShipped;
+
+// question 7
+var orderHasShipped = window.confirm("Click OK to get true. Click cancel to get false.");
+orderHasShipped;
+
+if (orderHasShipped === true) {
+    console.log ("You said true");
+}
+
+if (orderHasShipped === false) {
+    console.log ("You said false");
+}
+
+// question 8
+const names = ["Tim", "Petter", "Jørgen", "Eirik", "Anders", "Fredrik", "Trym", "Thomas", "Steinar"];
+
+for (name of names) {
+    console.log (name);
+}
